fb102d5d6928a7080b07d79a655b8384f2bf9946
djahid <djahidabdelmoumen@gmail.com>
2018-11-12 18:54:45 +0100

    TP6 exo1 algo

---
+++ TP_ALGO/TP6/Exercice2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int real;
+	int img;
+} Complex;
+
+void *swap(void* a, void* b) {
+	
+}
+
+Complex* sort() {
+	
+}
+
+int main() {
+
+}
